3dad4fb798bb22eb23c880607aa62b1ebb002dda
ProximalElk6186 <133893464+ProximalElk6186@users.noreply.github.com>
2025-01-06 17:08:43 +0200

    a

--- WinNT-MultiDisc-Installer/Form2.Designer.cs
+++ WinNT-MultiDisc-Installer/Form2.Designer.cs
@@ -43,9 +43,10 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe UI", 10F);
             this.label2.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.label2.Location = new System.Drawing.Point(15, 54);
+            this.label2.Location = new System.Drawing.Point(10, 35);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(788, 364);
+            this.label2.Size = new System.Drawing.Size(553, 247);
             this.label2.TabIndex = 3;
             this.label2.Text = resources.GetString("label2.Text");
             // 
@@ -54,9 +55,10 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.Highlight;
-            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Location = new System.Drawing.Point(8, 6);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(401, 45);
+            this.label1.Size = new System.Drawing.Size(273, 30);
             this.label1.TabIndex = 2;
             this.label1.Text = "Step 1: Partition the drives";
             // 
@@ -65,9 +67,10 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.SystemColors.Highlight;
-            this.label3.Location = new System.Drawing.Point(593, 130);
+            this.label3.Location = new System.Drawing.Point(420, 84);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(129, 45);
+            this.label3.Size = new System.Drawing.Size(88, 30);
             this.label3.TabIndex = 4;
             this.label3.Text = "list disk\r\n";
             // 
@@ -76,9 +79,10 @@
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Segoe UI Semibold", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.SystemColors.Highlight;
-            this.label4.Location = new System.Drawing.Point(593, 219);
+            this.label4.Location = new System.Drawing.Point(420, 148);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(198, 45);
+            this.label4.Size = new System.Drawing.Size(135, 30);
             this.label4.TabIndex = 5;
             this.label4.Text = "select disk #";
             // 
@@ -87,9 +91,10 @@
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Segoe UI Semibold", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.SystemColors.Highlight;
-            this.label5.Location = new System.Drawing.Point(88, 327);
+            this.label5.Location = new System.Drawing.Point(66, 214);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(96, 45);
+            this.label5.Size = new System.Drawing.Size(65, 30);
             this.label5.TabIndex = 6;
             this.label5.Text = "clean";
             // 
@@ -98,18 +103,20 @@
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Segoe UI Semibold", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label6.ForeColor = System.Drawing.SystemColors.Highlight;
-            this.label6.Location = new System.Drawing.Point(12, 418);
+            this.label6.Location = new System.Drawing.Point(8, 282);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(364, 675);
+            this.label6.Size = new System.Drawing.Size(247, 450);
             this.label6.TabIndex = 7;
             this.label6.Text = "convert gpt\r\n\r\ncreate part efi size=500\r\n\r\nformat fs=fat32 quick\r\n\r\nassign letter" +
     " w\r\n\r\ncreate part primary\r\n\r\nformat fs=ntfs quick\r\n\r\nassign letter Y\r\n\r\nexit";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(20, 113);
+            this.button1.Location = new System.Drawing.Point(13, 73);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(393, 30);
+            this.button1.Size = new System.Drawing.Size(262, 20);
             this.button1.TabIndex = 8;
             this.button1.Text = "Launch Diskpart";
             this.button1.UseVisualStyleBackColor = true;
@@ -117,9 +124,9 @@
             // 
             // Form2
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(818, 1110);
+            this.ClientSize = new System.Drawing.Size(566, 766);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
@@ -127,8 +134,9 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form2";
-            this.Text = "Partition the drives";
+            this.Text = "s";
             this.ResumeLayout(false);
             this.PerformLayout();
 
